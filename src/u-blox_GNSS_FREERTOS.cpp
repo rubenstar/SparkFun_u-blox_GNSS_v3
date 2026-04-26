1dd248d74d96f8b02a62d1774c5922ae4bc543b5
rubenstar <ruben-stadler@hotmail.com>
2026-04-26 20:22:47 +0200

    Mutex behavior now solved externally

--- src/u-blox_GNSS_FREERTOS.cpp
+++ src/u-blox_GNSS_FREERTOS.cpp
@@ -1,45 +1,12 @@
-#include <FreeRTOS.h>
-#include <semphr.h>
 #include <u-blox_GNSS_FREERTOS.h>
-
-bool SFE_UBLOX_GNSS_FREERTOS::createLock(void)
-{
-    xMutex = xSemaphoreCreateMutex();
-    return (xMutex != NULL);
-}
+#include <i2c.h>
 
 bool SFE_UBLOX_GNSS_FREERTOS::lock(void)
 {
-    if (xMutex != NULL)
-    {
-        return (xSemaphoreTake(xMutex, portMAX_DELAY) == pdPASS);
-    }
-    return false;
+    return i2c_lock();
 }
 
 void SFE_UBLOX_GNSS_FREERTOS::unlock(void)
 {
-    if (xMutex != NULL)
-    {
-        configASSERT(xSemaphoreGive(xMutex) == pdPASS);
-    }
-}
-
-void SFE_UBLOX_GNSS_FREERTOS::deleteLock(void)
-{
-    if (xMutex != NULL)
-    {
-        vSemaphoreDelete(xMutex);
-        xMutex = NULL;
-    }
-}
-
-bool SFE_UBLOX_GNSS_FREERTOS::publicLockI2C(void)
-{
-    return lock();
-}
-
-void SFE_UBLOX_GNSS_FREERTOS::publicUnlockI2C(void)
-{
-    return unlock();
+    i2c_unlock();
 }
--- src/u-blox_GNSS_FREERTOS.h
+++ src/u-blox_GNSS_FREERTOS.h
@@ -7,18 +7,9 @@
 
 class SFE_UBLOX_GNSS_FREERTOS : public SFE_UBLOX_GNSS
 {
-    private:
-        SemaphoreHandle_t xMutex = NULL;
-    
     protected:
-        bool createLock(void) override;
         bool lock(void) override;
         void unlock(void) override;
-        void deleteLock(void) override;
-
-    public:
-        bool publicLockI2C(void);
-        void publicUnlockI2C(void);
 };
 
 #endif // GNSS_FREERTOS_H
